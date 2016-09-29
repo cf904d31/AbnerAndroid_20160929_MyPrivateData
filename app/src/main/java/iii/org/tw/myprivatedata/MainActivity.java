fae1984921fcc61c99e6abd2d69770f7c06cc0b6
cf904d31 <cf904d31@gmail.com>
2016-09-29 14:16:14 +0800

    AbnerAndroid_20160929_MyPrivateData v1

--- app/src/main/java/iii/org/tw/myprivatedata/MainActivity.java
+++ app/src/main/java/iii/org/tw/myprivatedata/MainActivity.java
@@ -2,12 +2,17 @@ package iii.org.tw.myprivatedata;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TelephonyManager tmgr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        tmgr = (TelephonyManager)getSystemService(TELEPHONY_SERVICE);
     }
 }
