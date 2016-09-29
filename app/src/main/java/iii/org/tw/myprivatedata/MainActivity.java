c81ce2904bead47db2eae3951dcf4efe7212f1e4
cf904d31 <cf904d31@gmail.com>
2016-09-29 16:40:15 +0800

    AbnerAndroid_20160929_MyPrivateData v3(取出手機裡相片)

--- app/src/main/java/iii/org/tw/myprivatedata/MainActivity.java
+++ app/src/main/java/iii/org/tw/myprivatedata/MainActivity.java
@@ -6,8 +6,12 @@ import android.accounts.AccountManager;
 import android.content.ContentResolver;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Build;
 import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -17,16 +21,19 @@ import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     private TelephonyManager tmgr;
     private AccountManager amgr;
+    private ImageView showImg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showImg = (ImageView) findViewById(R.id.showImg);
 
-        if (Build.VERSION.SDK_INT > 23) {
+        if (Build.VERSION.SDK_INT >= 23) {
             if (ContextCompat.checkSelfPermission(this,
                     Manifest.permission.READ_CONTACTS)
                     != PackageManager.PERMISSION_GRANTED) {
@@ -51,9 +58,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         tmgr = (TelephonyManager)getSystemService(TELEPHONY_SERVICE);
+        String linenum = tmgr.getLine1Number();
         String imei = tmgr.getDeviceId();
         String imsi = tmgr.getSubscriberId();
-
+        if (linenum == "0963257345") {
+            linenum = "123";
+        } else {
+            linenum = "空的";
+        }
+        Log.d("Abner",linenum);
         Log.d("Abner",imei);
         Log.d("Abner",imsi);
 
@@ -91,16 +104,36 @@ public class MainActivity extends AppCompatActivity {
         String number = ContactsContract.CommonDataKinds.Phone.NUMBER;
         Cursor c = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI
                 ,new String[]{name,number},null,null,name);
-        if (c.moveToNext()) {
+        int count = c.getCount();
+        Log.d("Abner","Count:" + count);
+        while (c.moveToNext()) {
             String dname = c.getString(c.getColumnIndex(name));
             String dnumber = c.getString(c.getColumnIndex(number));
+
             Log.d("Abner",dname + ":" + dnumber);
         }
     }
     public void b2(View v) {
+        Uri sim = Uri.parse("content://icc/adn");
+        ContentResolver contentResolver = getContentResolver();
+        String name = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME;
+        String number = ContactsContract.CommonDataKinds.Phone.NUMBER;
+        Cursor c = contentResolver.query(sim,null,null,null,name);
+        int count = c.getCount();
+        Log.d("Abner","Count:" + count);
+        while (c.moveToNext()) {
+            String dname = c.getString(c.getColumnIndex("name"));
+            String dnumber = c.getString(c.getColumnIndex("number"));
 
+            Log.d("Abner",dname + ":" + dnumber);
+        }
     }
     public void b3(View v) {
-
+        ContentResolver contentResolver = getContentResolver();
+        Cursor c = contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,null,null,null,null);
+        c.moveToNext();
+        String photo = c.getColumnName(c.getColumnIndex(MediaStore.Images.Media.DATA));
+        Log.d("Abner",photo);
+        showImg.setImageBitmap(BitmapFactory.decodeFile(photo));
     }
 }
